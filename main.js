983c8a62d501e12c5dd6ba0a6c1a95b1ff734082
DUA-UMER <duam95623@gmail.com>
2024-03-30 17:50:13 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+"use strict";
+//varaiable decleration//
+// var fname = "rimsha"
+// let username ="malik rimsha"
+// const PI:number = 3.14
+// // console.log( fname, username, PI);
+//   //type Annotations//
+// let age:number = 24;
+// let istudent:boolean = true
+// console.log( age,istudent);
+//  // type inference //
+// let country = "pakistan"
+//  console.log(country);
+//  //using any type//
+// let data:any =400
+// data = "four hundred"
+// data = true
+// console.log( data,);
+// // block level scoping//
+if (true) {
+    let blockscopevar = "i m block scope";
+    console.log(blockscopevar);
+}
+//no hoisting with let//
+var firstname = "rimsha";
+console.log(firstname);
+const account = "2546124ffdv";
+console.log(account);
---
+++ main.ts
@@ -0,0 +1,46 @@
+ 
+ 
+ //varaiable decleration//
+
+var fname = "rimsha"
+let username ="malik rimsha"
+const PI:number = 3.14
+
+ console.log( fname, username, PI);
+
+
+//   //type Annotations//
+
+let age:number = 24;
+let istudent:boolean = true
+
+console.log( age,istudent);
+
+  // type inference //
+
+let country = "pakistan"
+ console.log(country);
+
+//  //using any type//
+
+let data:any =400
+data = "four hundred"
+data = true
+
+console.log( data,);
+
+// block level scoping//
+
+if (true){
+    let blockscopevar = "i m block scope"
+        console.log(blockscopevar)
+    }
+
+// //no hoisting with let//
+    
+var firstname = "rimsha"
+console.log(firstname)
+
+const account: string = "2546124ffdv"
+ console.log(account);
+
